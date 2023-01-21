b9b4867f9dee3c5455da2b36a393d4464cbbcbcb
kaceykniffen <kaceykniffen@gmail.com>
2023-01-20 18:39:28 -0600

    Finished Layout of Body Page

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -14,41 +14,40 @@ const Body: Component = () => {
           <a href="#">Start Here</a>
         </div>
         <div class={styles.videoDiv}>
-          <video width={420}  controls>
-            <source src="/videos/solid-in-100-seconds.mp4" type="video/mp4" />
-            Video could not load properly.
-          </video>
+          <iframe src={"https://player.vimeo.com/video/599890291"}
+            allowfullscreen
+          />  
         </div>
       </section>
-      <section>
+      <section class={styles.hardTruth}>
         <h2>Hard Truth</h2>
-        <a>👇</a>
+        <a href="#truth">👇</a>
       </section>
-      <section>
-        <p>You <span>Can't</span> learn to code by watching videos</p>
-        <a>i'm sad 😢</a>
+      <section id="truth" class={`${styles.bodySection}`}>
+        <p class={styles.cantLearn}>You <span class={styles.boldText}>Can't</span> learn to code by watching videos</p>
+        <a href="#solution">i'm sad 😢</a>
       </section>
-      <section>
+      <section id="solution" class={styles.bodySection} style={{"--bold-color": "rgb(34 197 94)"}}>
         <h2>Solution</h2>
-        <p>Fireship is all about <span>Project-based</span> learning.
-        I create short highly-focused videos that make learning to code addicting. </p>
-        <a>but like how? 🤔</a>
+        <p>Fireship is all about <span class={styles.boldText}>Project-based</span> learning.
+          I create short highly-focused videos that make learning to code addicting. </p>
+        <a href="#fun">but like how? 🤔</a>
       </section>
-      <section>
+      <section id="fun" class={styles.bodySection} style={{"--bold-color": "rgb(168 85 247)"}}>
         <h2>Have Fun</h2>
         <p>
           I built this one-of-a-kind learning platform to give
-          you <span>Dopamine</span> hits like XP and meme prizes
+          you <span class={styles.boldText}>Dopamine</span> hits like XP and meme prizes
           for every little accomplishment
         </p>
-        <button>login now to start earning useless XP points 💎</button>
+        <button class={styles.loginBtn}>login now to start earning useless XP points 💎</button>
       </section>
-      <section>
+      <section id="go" class={styles.bodySection} style={{"--bold-color": "rgb(234 179 8)"}}>
         <h2>Let's Go</h2>
         <p>
-          There's tons of free content, here, so <span>Play</span> around before you upgrade
+          There's tons of free content, here, so <span class={styles.boldText}>Play</span> around before you upgrade
         </p>
-        <a>every course starts out free</a>
+        <a href="/">every course starts out free</a>
       </section>
     </main>
   )
